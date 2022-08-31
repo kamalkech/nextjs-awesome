0b1eadf5f6c0367e8e1668a8822ccda6082d9f99
kamalkech <maroc.develop@gmail.com>
2022-08-31 18:39:05 +0100

    .

--- src/server/env.js
+++ src/server/env.js
@@ -3,21 +3,21 @@
  * This file is included in `/next.config.js` which ensures the app isn't built with invalid env vars.
  * It has to be a `.js`-file to be imported there.
  */
-const { z } = require('zod')
+const { z } = require("zod");
 
 /*eslint sort-keys: "error"*/
 const envSchema = z.object({
   DATABASE_URL: z.string().url(),
-  NODE_ENV: z.enum(['development', 'test', 'production']),
-})
+  NODE_ENV: z.enum(["development", "test", "production"]),
+});
 
-const env = envSchema.safeParse(process.env)
+const env = envSchema.safeParse(process.env);
 
 if (!env.success) {
   console.error(
-    '❌ Invalid environment variables:',
+    "❌ Invalid environment variables:",
     JSON.stringify(env.error.format(), null, 4)
-  )
-  process.exit(1)
+  );
+  process.exit(1);
 }
-module.exports.env = env.data
+module.exports.env = env.data;
