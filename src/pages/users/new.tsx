9894fdf8f964113973a44de4e9f285d1fd93efd6
kamalkech <maroc.develop@gmail.com>
2022-09-01 23:38:00 +0100

    .

--- src/pages/users/new.tsx
+++ src/pages/users/new.tsx
@@ -1,57 +1,81 @@
 import React, { useState } from "react";
+import { Formly, type IField, type IValue } from "formly-react";
+import { trpc } from "~/utils/trpc";
 
 const classes =
   "mb-4 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500";
+const classesSubmit =
+  "text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800";
 
 const New = () => {
-  const [username, setUsername] = useState<string>("");
-  const [email, setEmail] = useState<string>("");
-  const [password, setPassword] = useState<string>("");
+  const utils = trpc.useContext();
+  const usersQuery = trpc.useQuery(["user.all"]);
+  const mutation = trpc.useMutation("signup", {
+    async onSuccess() {
+      // refetches posts after a post is added
+      await utils.invalidateQueries(["user.all"]);
+    },
+  });
 
-  const onSubmitHandler = async () => {
-    console.log(111);
+  const fields: IField[] = [
+    {
+      type: "input",
+      name: "username",
+      value: "streamer",
+      attributes: {
+        id: "username",
+        placeholder: "username",
+        classes: [classes],
+        autocomplete: "off",
+      },
+      rules: ["required", "min:4"],
+    },
+    {
+      type: "input",
+      name: "email",
+      value: "streamer@email.com",
+      attributes: {
+        type: "email",
+        id: "email",
+        placeholder: "email",
+        classes: [classes],
+        autocomplete: "off",
+      },
+      rules: ["required", "min:6"],
+    },
+    {
+      type: "input",
+      name: "password",
+      value: "streamer",
+      attributes: {
+        type: "password",
+        id: "password",
+        placeholder: "password",
+        classes: [classes],
+        autocomplete: "off",
+      },
+      rules: ["required", "min:6"],
+    },
+  ];
+
+  // * Handler on submit.
+  const onSubmit = async (data: IValue) => {
+    if (data.valid) {
+      await mutation.mutateAsync(data);
+    }
   };
+
   return (
     <>
-      <ul>
-        <li>{username}</li>
-        <li>{email}</li>
-        <li>{password}</li>
-      </ul>
-      <form onSubmit={onSubmitHandler}>
-        <input
-          type="text"
-          name="username"
-          id="username"
-          className={classes}
-          placeholder="username"
-          onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-            setUsername(e.target.value)
-          }
-        />
-        <input
-          type="email"
-          name="email"
-          id="email"
-          className={classes}
-          placeholder="email"
-          onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-            setEmail(e.target.value)
-          }
-        />
-        <input
-          type="password"
-          name="password"
-          id="password"
-          className={classes}
-          placeholder="email"
-          onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-            setPassword(e.target.value)
-          }
-        />
-
-        <button type="submit">signup</button>
-      </form>
+      <Formly
+        fields={fields}
+        form_name={"signup"}
+        onSubmit={onSubmit}
+        btnSubmit={{
+          text: "Send",
+          classes: [classesSubmit],
+        }}
+      />
     </>
   );
 };
